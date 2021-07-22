a749e76cbab3bffd8a86a23004d5dbb2ab4b110b
psooin <psooin3356@gmail.com>
2021-07-22 15:10:21 +0900

    [박수인]메인Controller 날씨 수정

--- spring_ilchooL/src/main/java/project/spring/ilchooL/controllers/MainController.java
+++ spring_ilchooL/src/main/java/project/spring/ilchooL/controllers/MainController.java
@@ -1,5 +1,6 @@
 package project.spring.ilchooL.controllers;
 
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,30 +9,77 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
 import project.spring.ilchooL.helper.RetrofitHelper;
 import project.spring.ilchooL.helper.WebHelper;
 import project.spring.ilchooL.model.Covid19Item;
 import project.spring.ilchooL.model.CovidItem;
+import project.spring.ilchooL.model.Witem;
 import project.spring.ilchooL.service.CovidService;
+import project.spring.ilchooL.service.WeatherService;
+
 
 /**
  * 메인 페이지 컨트롤러
  */
+
+@Slf4j
 @Controller
 public class MainController {
-	@Autowired
-	WebHelper webHelper;
-	@Autowired
-	RetrofitHelper retrofitHelper;
+	@Autowired WebHelper webHelper;
+	@Autowired RetrofitHelper retrofitHelper;
 	@Autowired CovidService covidService;
+	@Autowired WeatherService weatherService;
 
-	/**
-	 * 날씨, 미세먼지, 코로나19, 그래프 영역은 메인컨트롤러에서 구현하면 될것같아요!
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-		// Retrofit 객체 선언 부분
-		// 이부분에 수인님 코드 추가
+		
+		/** 날씨 데이터 조회 */
+		List<Witem> w_item_list = null;
+		
+		//데이터 조회
+		try {
+			w_item_list = weatherService.getItemList();
+			for (Witem item : w_item_list) {
+				System.out.println(">>>>>>>>>>>>>>>>>>>");
+				System.out.println(item.toString());
+			}
+		} catch (Exception e) {
+			log.error(e.getLocalizedMessage());
+			e.printStackTrace();
+		}
+
+		String ca = null;
+		String da = null;
+		int sky = 0;
+		int tmp = 0;
+		
+		Calendar date = Calendar.getInstance();
+		String w_time = String.format("%02d00", date.get(Calendar.HOUR_OF_DAY));
+		
+		//System.out.println(w_time);
+
+		for (int i = 0; i < w_item_list.size(); i++) {
+			ca = w_item_list.get(i).getCategory();
+			da = w_item_list.get(i).getFcst_time();
+			
+			// 'SKY'값 조회
+			if (ca.equals("SKY") && da.equals(w_time)) {
+				//System.out.println("SKY :" + w_item_list.get(i).getFcst_value());
+				sky = Integer.parseInt(w_item_list.get(i).getFcst_value());
+				//System.out.println(sky);
+			} 
+			
+			// 'TMP'값 조회
+			if (ca.equals("TMP") && da.equals(w_time)) {
+				//System.out.println("TMP :" + w_item_list.get(i).getFcst_value());
+				tmp = Integer.parseInt(w_item_list.get(i).getFcst_value());
+				//System.out.println(tmp);
+			}
+		}
+		
+		model.addAttribute("sky", sky);
+		model.addAttribute("tmp", tmp);
 		
 		// Service 인터페이스 선언부분
 		// 이부분에 수인님 코드 추가
