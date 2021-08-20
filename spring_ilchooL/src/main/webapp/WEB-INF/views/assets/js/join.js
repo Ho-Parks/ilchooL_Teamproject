76b0d10e37fd936d63adea0ee59bdcf63b6fe6fc
inhopark <ebusinho@gmail.com>
2021-08-21 04:30:16 +0900

    [박인호] 회원가입 js 수정

--- spring_ilchooL/src/main/webapp/WEB-INF/views/assets/js/join.js
+++ spring_ilchooL/src/main/webapp/WEB-INF/views/assets/js/join.js
@@ -40,18 +40,18 @@ mobile.addEventListener("focusout", checkPhoneNum);
 function checkPw() {
 	var pwPattern = /[a-zA-Z0-9~!@#$%^&*()_+|<>?:{}]{8,16}/;
 	if (pw1.value == "") {
-		error[1].innerHTML = "필수 정보입니다.";
-		error[1].style.display = "block";
+		error[0].innerHTML = "필수 정보입니다.";
+		error[0].style.display = "block";
 	} else if (!pwPattern.test(pw1.value)) {
-		error[1].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+		error[0].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
 		pwMsg.innerHTML = "사용불가";
 		pwMsgArea.style.paddingRight = "93px";
-		error[1].style.display = "block";
+		error[0].style.display = "block";
 
 		pwMsg.style.display = "block";
 		pwImg1.src = "../assets/img/login_new_not_use.png";
 	} else {
-		error[1].style.display = "none";
+		error[0].style.display = "none";
 		pwMsg.innerHTML = "안전";
 		pwMsg.style.display = "block";
 		pwMsg.style.color = "#03c75a";
@@ -62,29 +62,29 @@ function checkPw() {
 function comparePw() {
 	if (pw2.value == pw1.value && pw2.value != "") {
 		pwImg2.src = "../assets/img/login_new_check_enable.png";
-		error[2].style.display = "none";
+		error[1].style.display = "none";
 	} else if (pw2.value !== pw1.value) {
 		pwImg2.src = "../assets/img/login_new_check_not.png";
-		error[2].innerHTML = "비밀번호가 일치하지 않습니다.";
-		error[2].style.display = "block";
+		error[1].innerHTML = "비밀번호가 일치하지 않습니다.";
+		error[1].style.display = "block";
 	}
 
 	if (pw2.value == "") {
-		error[2].innerHTML = "필수 정보입니다.";
-		error[2].style.display = "block";
+		error[1].innerHTML = "필수 정보입니다.";
+		error[1].style.display = "block";
 	}
 }
 
 function checkName() {
 	var namePattern = /^[ㄱ-ㅎ가-힣]*$/;
 	if (userName.value == "") {
-		error[3].innerHTML = "필수 정보입니다.";
-		error[3].style.display = "block";
+		error[2].innerHTML = "필수 정보입니다.";
+		error[2].style.display = "block";
 	} else if (!namePattern.test(userName.value) || userName.value.indexOf(" ") > -1) {
-		error[3].innerHTML = "한글만 사용하세요. (특수기호, 공백 사용 불가)";
-		error[3].style.display = "block";
+		error[2].innerHTML = "한글만 사용하세요. (특수기호, 공백 사용 불가)";
+		error[2].style.display = "block";
 	} else {
-		error[3].style.display = "none";
+		error[2].style.display = "none";
 	}
 }
 
